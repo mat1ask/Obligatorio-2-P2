f5ce3c709ee05f01e9ef742431b289e321271364
mat1ask <136759965+mat1ask@users.noreply.github.com>
2023-10-26 17:02:17 -0300

    a

---
+++ Obligatorio2/src/Dominio/Evaluador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dominio;
+
+/**
+ *
+ * @author matiaskunin
+ */
+public class Evaluador {
+    String nombre;
+    int cedula;
+    String direccion;
+    int ingreso;
+}
---
+++ Obligatorio2/src/Dominio/Postulante.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+import java.util.*;
+
+public class Postulante {
+    String nombre;
+    int cedula;
+    String direccion;
+    int telefono;
+    String mail;
+    String linkedin;
+    String formato;
+    HashMap temas;
+}
