dea44150746fdfa762749056e28f263542acc43b
IgnacioEtchevers <nachovers23@gmail.com>
2023-10-31 20:27:29 -0300

    a
    a

--- Obligatorio2/src/Dominio/Evaluador.java
+++ Obligatorio2/src/Dominio/Evaluador.java
@@ -6,6 +6,13 @@ public class Evaluador {
     int cedula;
     String direccion;
     int anoIngreso;
+    
+    public Evaluador(String nombre, int cedula, String direccion, int anoIngreso) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.direccion = direccion;
+        this.anoIngreso = anoIngreso;
+    }
 
     public String getNombre() {
         return nombre;
@@ -22,5 +29,20 @@ public class Evaluador {
     public int getAnoIngreso() {
         return anoIngreso;
     }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    public void setAnoIngreso(int anoIngreso) {
+        this.anoIngreso = anoIngreso;
+    }
 }
